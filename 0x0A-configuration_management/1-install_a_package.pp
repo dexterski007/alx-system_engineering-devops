48a2d74b9db66aa9a0845e0a271467d126a5319d
Dexterski007 <mbouj007@gmail.com>
2024-04-19 12:03:29 -0700

    exec

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 # puppet manifest for installing a package
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => 'pip3 install flask==2.1.0',
+  path        => '/usr/local/bin:/usr/bin:/bin',
+  refreshonly => true,
+  subscribe   => Package['python3-pip'],
 }
