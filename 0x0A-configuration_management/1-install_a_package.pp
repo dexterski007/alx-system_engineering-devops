7bdf5e8fa59b26f2a81c813357e6d5086aa95899
Dexterski007 <mbouj007@gmail.com>
2024-04-19 12:05:42 -0700

    lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,6 @@
 # puppet manifest for installing a package
-exec { 'install_flask':
-  command     => 'pip3 install flask==2.1.0',
+
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
