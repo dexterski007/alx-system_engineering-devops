e7992fd30000724e10d7880b7c34f15c76aa7356
Dexterski007 <mbouj007@gmail.com>
2024-04-20 03:06:01 -0700

    puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,15 @@
 # puppet config for ssh
 
 file_line {'disable pass auth':
-  path  => '/etc/ssh/sshd_config'
-  line  => 'PasswordAuthentication no'
-  match => ''
-
+  ensure  => present,
+  path    => '/etc/ssh/sshd_config',
+  line    => 'PasswordAuthentication no',
+  match   => '^#?PasswordAuthentication',
+  replace => true,
+}
+file_line {'disable pass auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
 }
