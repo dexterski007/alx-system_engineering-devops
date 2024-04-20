bad690e37b1066a7b8ee19e39785f553408f1623
Dexterski007 <mbouj007@gmail.com>
2024-04-20 14:48:46 -0700

    check

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,6 @@
 # Seting up my client config file
-augeas { 'disable password auth':
-  context => '/files/etc/ssh/ssh_config',
-  changes => "set *[. = 'PasswordAuthentication yes'] 'PasswordAuthentication no'",
-}
 
-augeas { 'Delare identity file':
-  context => '/files/etc/ssh/ssh_config',
-  changes => [
-    "ins line before /files/etc/ssh/ssh_config/*[1]",
-    "set /files/etc/ssh/ssh_config/*[1] 'IdentityFile ~/.ssh/school'",
-  ],
+exec { 'sshconfig':
+  path    => '/usr/bin/',
+  command => 'echo "IdentityFile ~/.ssh/school\n PasswordAuthentication no" >> /etc/ssh/ssh_config',
 }
