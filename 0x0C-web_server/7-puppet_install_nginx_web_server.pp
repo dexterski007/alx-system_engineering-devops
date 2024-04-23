f3d351a4357b1e7b3de441133222abdfe3e29cbf
Dexterski007 <mbouj007@gmail.com>
2024-04-23 13:01:54 -0700

    reboot

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,18 +16,12 @@ file { '/var/www/html/index.nginx-debian.html':
   require => Package['nginx'],
 }
 
-exec { 'nginx-redirect':
-  command => '/bin/sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-}
-
 exec { 'nginx-restart':
   command     => '/bin/systemctl restart nginx',
   refreshonly => true,
   subscribe   => File['/var/www/html/index.nginx-debian.html'],
 }
 
-exec { 'nginx-restart':
-  command     => '/bin/systemctl restart nginx',
-  refreshonly => true,
-  subscribe   => File['/etc/nginx/sites-available/default'],
+exec { 'nginx-reboot':
+  command => '/bin/systemctl restart nginx',
 }
