2c52ca153b9f56c233c58bfd06e4bd45f16ba491
Dexterski007 <mbouj007@gmail.com>
2024-04-23 15:33:32 -0700

    fina

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -21,8 +21,8 @@ file { '/etc/nginx/sites-available/default':
   content => "
 server {
     listen 80 default_server;
-    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
     listen [::]:80 default_server;
+    rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
     root /var/www/html;
 
     index index.html index.htm index.nginx-debian.html;
