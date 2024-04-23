73399876f1a8ea80ec3624032b8a76e3cfbaecc0
Dexterski007 <mbouj007@gmail.com>
2024-04-23 13:10:00 -0700

    root

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -18,8 +18,10 @@ file { '/var/www/html/index.nginx-debian.html':
 
 exec { 'nginx-redirect':
   command => '/bin/sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  user    => 'root',
 }
 
 exec { 'nginx-restart':
-  command     => '/bin/systemctl restart nginx',
+  command => '/bin/systemctl restart nginx',
+  user    => 'root',
 }
