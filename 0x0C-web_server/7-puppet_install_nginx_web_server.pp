48f25fc50a36b282d154689e7a797561ea5293c2
Dexterski007 <mbouj007@gmail.com>
2024-04-23 12:23:24 -0700

    f

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,6 @@
 # puppet nginx server configurator
 
-exec {'serverconfig':
+exec {'install nginx':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent; /etc/nginx/sites-available/default" ; sudo systemctl restart nginx';
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
 }
