d86f3371df766a74c3a90ae0efc693c7f3ab3ce2
Dexterski007 <mbouj007@gmail.com>
2024-04-23 12:24:07 -0700

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec {'install nginx':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ;
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx';
 }
