b6b0509b2bc9f71df1ef34d0adadd27242d5235f
Dexterski007 <mbouj007@gmail.com>
2024-04-23 13:54:57 -0700

    lkawn

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,36 +1,53 @@
 # puppet nginx server configurator
 
-class setup_nginx {
-  package { 'nginx':
-    ensure  => installed,
-  }
-
-  service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx'],
-  }
-
-  nginx::resource::server { 'default_server':
-    listen_port => 80,
-    server_name => '_',
-  }
-
-  nginx::resource::location { 'default_server_redirect':
-    ensure          => present,
-    location        => '/redirect_me/',
-    server          => 'default_server',
-    location_config => {
-      'return' => '301 https://www.theroom.com/;',
-    },
-    require         => Nginx::Resource::Server['default_server'],
-  }
-
-  file { '/usr/share/nginx/html/index.html':
-    ensure  => present,
-    content => 'Hello World!',
-    require => Package['nginx'],
-  }
+package { 'nginx':
+  ensure => installed,
 }
 
-include setup_nginx
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => Package['nginx'],
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  owner   => 'root',
+  group   => 'root',
+  content => "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}
+",
+  notify  => Exec['nginx-restart'],
+}
+
+exec { 'nginx-restart':
+  command     => '/bin/systemctl restart nginx',
+  refreshonly => true,
+  subscribe   => [
+  File['/etc/nginx/sites-available/default'],
+  File['/var/www/html/index.nginx-debian.html'],
+],
+}
