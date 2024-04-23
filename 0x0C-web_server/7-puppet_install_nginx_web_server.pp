d031619b55cb2aa52b6c70d1af37e3ea87698b2b
Dexterski007 <mbouj007@gmail.com>
2024-04-23 13:21:20 -0700

    redi

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -16,9 +16,6 @@ file { '/var/www/html/index.nginx-debian.html':
   require => Package['nginx'],
 }
 
-file_line { 'redirection-301':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+exec { 'redirect':
+  command => "/usr/bin/sed -i '/^}$/i \\\n\tlocation \\/redirect_me {return 301 https:\\/\\/www.youtube.com\\/watch?v=QH2-TGUlwu4;}' /etc/nginx/sites-available/default",
 }
