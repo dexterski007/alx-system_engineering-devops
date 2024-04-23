69ce9e0bef6a133e0960fd20824223462d8d12ba
Dexterski007 <mbouj007@gmail.com>
2024-04-23 12:40:49 -0700

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -9,3 +9,15 @@ service { 'nginx':
   enable  => true,
   require => Package['nginx'],
 }
+
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec { 'nginx-restart':
+  command     => '/bin/systemctl restart nginx',
+  refreshonly => true,
+  subscribe   => File['/var/www/html/index.nginx-debian.html'],
+}
