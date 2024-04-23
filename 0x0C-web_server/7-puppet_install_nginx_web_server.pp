8d0acadc9d4213aef2ae8a6607b1148cffea9d66
Dexterski007 <mbouj007@gmail.com>
2024-04-23 12:08:40 -0700

    seccondtry

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,6 @@
 # puppet nginx server configurator
 
-package {'nginx':
-  ensure => installed;
-}
-
-service {'nginx':
-  require => Package['nginx'];
-  ensure  => running;
-  enable  => true;
-}
-
-exec {'create homepage':
-  command => 'echo "Hello World!" > /var/www/html/index.nginx-debian.html';
-  path    => ['/bin', '/usr/bin'];
-}
-
-exec {'create redirection':
-  command => 'sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-available/default';
-  path    => ['/bin', '/usr/bin'];
-}
-
-exec {'reboot':
-  command => 'systemctl restart nginx';
-  path    => ['/bin', '/usr/bin'];
+exec {'serverconfig':
+  provider => shell,
+  command  => 'sudo apt-get -y update ; sudo apt-get -y upgrade ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /vat/www/html/index.nginx-debian.html ; sudo sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default" ; sudo systemctl restart nginx';
 }
