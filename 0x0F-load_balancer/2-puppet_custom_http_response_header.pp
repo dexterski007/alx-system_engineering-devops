f90b03bc4172abe855ff555a5d2b1a660336c8e0
Dexterski007 <mbouj007@gmail.com>
2024-04-29 12:12:27 -0700

    test

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,34 +4,14 @@ package { 'nginx':
   ensure => installed,
 }
 
+file { '/etc/nginx/conf.d/custom_http_header.conf':
+  ensure  => present,
+  content => "add_header X-Served-By \$hostname;\n",
+  notify  => Service['nginx'],
+}
+
 service { 'nginx':
   ensure  => running,
   enable  => true,
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'present',
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By \$hostname;
-    root /var/www/html;
-
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}
-",
-  notify  => Exec['nginx-restart'],
-}
-
-exec { 'nginx-restart':
-  command   => '/bin/systemctl restart nginx',
-  subscribe => File['/etc/nginx/sites-available/default'],
+  require => File['/etc/nginx/conf.d/custom_http_header.conf'],
 }
