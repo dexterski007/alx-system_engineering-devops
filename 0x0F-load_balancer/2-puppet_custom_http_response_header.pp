2ea11cbfdcaab7f39b8cd480f29f0032ffba004a
Dexterski007 <mbouj007@gmail.com>
2024-04-29 12:23:04 -0700

    hey

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -13,10 +13,9 @@ exec {'install Nginx':
 }
 
 exec { 'add_header':
-  provider    => shell,
-  environment => ["HOST=${hostname}"],
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
-  before      => Exec['restart Nginx'],
+  provider => shell,
+  command  => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"\$hostname\";/" /etc/nginx/nginx.conf',
+  before   => Exec['restart Nginx'],
 }
 
 exec { 'restart Nginx':
