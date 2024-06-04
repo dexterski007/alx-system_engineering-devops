36c497476c61410bb03f58e2954ba3c149c20bba
dexterski007 <mbouj007@gmail.com>
2024-06-04 06:27:07 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,7 +14,6 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        print(data)
         return data['data']['subscribers']
     else:
         return 0
