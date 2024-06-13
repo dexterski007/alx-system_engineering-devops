c97d8cc51e4abfe3e348f6ab4b63aa14d753c6fe
dexterski007 <mbouj007@gmail.com>
2024-06-13 03:22:26 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 #fix for nginx multitasking
 exec { 'sedscript':
-  command => 'sed -i "s/worker_processes 4;/worker_processes 10;/g" /etc/nginx/nginx.conf',
+  command => "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 20000\"/g' /etc/default/nginx",
   path    => ['/usr/bin', '/usr/sbin', '/bin'],
-  onlyif  => 'grep -q "worker_processes 4;" /etc/nginx/nginx.conf',
+  onlyif  => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
   notify  => Service['nginx'],
 }
 
